7e1ae08482c286670eeea20c388185d3f9ddaf93
mac718 <mac718@gmail.com>
2017-11-28 13:45:13 -0500

    wrote hit functionality

--- app.rb
+++ app.rb
@@ -3,10 +3,40 @@ require 'erb'
 require 'json'
 require 'pry'
 
+enable :sessions
+
+helpers do 
+  def create_deck
+    ['A', 2, 3, 4, 5, 6, 7, 8, 9 ,10, 'J', 'Q', 'K'].product(['S', 'C', 'D', 'H'])
+  end
+  def deal_hand
+    session[:deck] = create_deck.shuffle
+    hand = []
+    2.times do 
+      hand << session[:deck].pop
+    end
+    hand
+  end
+  def hit
+    session[:deck].pop
+  end
+  def calculate_total
+
+  end
+end
+
 get '/' do 
   erb :home
 end
 
 get '/blackjack'do
+  session[:player_hand] = deal_hand
+  session[:computer_hand] = deal_hand
+  erb :blackjack
+end
+
+post '/blackjack/hit' do 
+  session[:player_hand] << hit
+  binding.pry
   erb :blackjack
 end
